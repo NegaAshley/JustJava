3f554d8244f453d09a193d13c8b3be65e76edbca
NegaAshley <nollam01@students.ipfw.edu>
2018-04-04 13:39:13 -0400

    First Just Java commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,82 @@
+
+/**
+ * IMPORTANT: Make sure you are using the correct package name.
+ * This example uses the package name:
+ * package com.example.android.justjava
+ * If you get an error when copying this code into Android studio, update it to match teh package name found
+ * in the project's AndroidManifest.xml file.
+ **/
+
+package com.example.android.justjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
+    double coffeePrice = 2.00;
+    double total = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        resetToStart();
+        display(quantity);
+        displayPrice(total);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(double number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view){
+        quantity += 1;
+        updatePrice();
+        display(quantity);
+        displayPrice(total);
+    }
+
+    public void decrement(View view){
+        if(quantity == 0){
+            return;
+        }
+        quantity -= 1;
+        updatePrice();
+        display(quantity);
+        displayPrice(total);
+    }
+
+    public void updatePrice(){
+        total = (quantity * coffeePrice);
+    }
+
+    public void resetToStart(){
+        quantity = 0;
+        updatePrice();
+    }
+}
