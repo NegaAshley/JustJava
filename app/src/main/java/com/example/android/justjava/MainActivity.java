492a0b23fb0d0d491225ed63c6892729f8e49f95
NegaAshley <nollam01@students.ipfw.edu>
2018-04-09 16:45:40 -0400

    Added thank you/error message functionality.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -24,37 +24,33 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        if(quantity == 0){
-            displayThanks("You must have more than zero coffees for an order.");
-            return;
-        }
-        displayThanks("Thank you for your order of " + quantity + " cup(s) of coffee for " +
-                NumberFormat.getCurrencyInstance().format(total) + "!");
+
+        createOrderSummary();
         resetToStart();
-        display(quantity);
+        displayQuantity(quantity);
         displayPrice(total);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int numCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numCoffees);
     }
 
     /**
      * This method displays the given price on the screen.
      */
     private void displayPrice(double number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     public void increment(View view){
         quantity += 1;
-        updatePrice();
-        display(quantity);
+        calculatePrice();
+        displayQuantity(quantity);
         displayPrice(total);
     }
 
@@ -63,33 +59,41 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
         quantity -= 1;
-        updatePrice();
-        display(quantity);
+        calculatePrice();
+        displayQuantity(quantity);
         displayPrice(total);
     }
 
-    public void updatePrice(){
+    public void calculatePrice(){
         total = (quantity * coffeePrice);
     }
 
     public void resetToStart(){
         quantity = 0;
-        updatePrice();
+        calculatePrice();
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
      * This method displays a thank you message after ordering.
      */
-    private void displayThanks(String givenString) {
+    private void createOrderSummary() {
         TextView thanksTextView = (TextView) findViewById(R.id.thank_you_text_view);
-        thanksTextView.setText(givenString);
+        String givenText;
+        if(quantity == 0){
+            givenText = "You must have more than zero coffees for an order.";
+            thanksTextView.setText(givenText);
+            return;
+        }
+        givenText = "Name: Kaptain Kunal\nQuantity: " + quantity + "\nTotal: " +
+                NumberFormat.getCurrencyInstance().format(total) + "\nThank you!";
+        thanksTextView.setText(givenText);
     }
 }
